62f88ae0763a2803d0ca87a7c44764920195ccca
sanches3020 <evmenovasofia7@gmail.com>
2025-11-18 09:32:17 +0300

    Update PsychologistSchedule.cs

--- Models/PsychologistSchedule.cs
+++ Models/PsychologistSchedule.cs
@@ -5,19 +5,19 @@ namespace Sofia.Web.Models;
 public class PsychologistSchedule
 {
     public int Id { get; set; }
-    
+
     public int PsychologistId { get; set; }
-    
+
     public DayOfWeek DayOfWeek { get; set; }
-    
+
     public TimeSpan StartTime { get; set; }
-    
+
     public TimeSpan EndTime { get; set; }
-    
+
     public bool IsAvailable { get; set; } = true;
-    
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    
+
+    public DateTime CreatedAt { get; set; }
+
     // Navigation properties
     public virtual Psychologist Psychologist { get; set; } = null!;
 }
@@ -25,23 +25,23 @@ public class PsychologistSchedule
 public class PsychologistTimeSlot
 {
     public int Id { get; set; }
-    
+
     public int PsychologistId { get; set; }
-    
+
     public DateTime Date { get; set; }
-    
+
     public TimeSpan StartTime { get; set; }
-    
+
     public TimeSpan EndTime { get; set; }
-    
+
     public bool IsAvailable { get; set; } = true;
-    
+
     public bool IsBooked { get; set; } = false;
-    
+
     public int? BookedByUserId { get; set; }
-    
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    
+
+    public DateTime CreatedAt { get; set; }
+
     // Navigation properties
     public virtual Psychologist Psychologist { get; set; } = null!;
     public virtual User? BookedByUser { get; set; }
